0057e03585418047ae04c77d1a0216fc11a0c689
Pawel Gozdz <pawel.gozdz.web.dev@gmail.com>
2018-11-10 13:01:00 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { DropdownComponent } from './dropdown/dropdown.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { BookshelfComponent } from './bookshelf/bookshelf.component';
+import { BookComponent } from './book/book.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    DropdownComponent,
+    NavigationComponent,
+    BookshelfComponent,
+    BookComponent
   ],
   imports: [
     BrowserModule
